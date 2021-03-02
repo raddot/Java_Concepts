fa11d621cd2267bfbc2e16de2f4ce62e07482f70
Radhika Dotihal <radhikakatraj@gmail.com>
2021-03-01 21:15:48 -0500

    Add files via upload

---
+++ variablesAssignment/VariablesAssignment.java
@@ -0,0 +1,19 @@
+package assignments.variablesAssignment;
+
+public class VariablesAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int intvar=10;
+		float floatvar=20.54f;
+		char charvar='x';
+		String strvar="I am the best";
+		
+		System.out.println(intvar);
+		System.out.println(floatvar);
+		System.out.println(charvar);
+		System.out.println(strvar);
+	}
+
+}
